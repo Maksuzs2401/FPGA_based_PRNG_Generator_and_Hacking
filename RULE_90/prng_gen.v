6638c55d62002346bc2a319c4e2187e379a88af0
Sujal Makwana <114184382+Maksuzs2401@users.noreply.github.com>
2026-02-07 23:14:26 +0530

    prng_gen.v

--- RULE_90/prng_gen.v
+++ RULE_90/prng_gen.v
@@ -2,37 +2,31 @@ module prng_gen(
     input wire clk,
     input wire reset,
     input [63:0] data_in,
-    input wire rx_valid_pulse, // Only step/load when this pulses
-    output wire [63:0] q_out,  // SEND THIS TO SPI
-    output wire led_sig
+    input wire rx_valid_pulse,  // data valid signal
+	output wire [63:0] q_out,  // final output to the rp2040
+    output wire led_sig    
 );
 
     reg [63:0] q;
     reg output_flag;
-   /* wire [63:0] w_left, w_right;
-    assign w_right = {q[62:0], 1'b0};
-    assign w_left = {1'b0, q[63:1]};*/
 
-    // Send the internal state out to the top module
     always @(posedge clk or negedge reset) begin
         if(!reset) begin
             q <= 64'd0;
             output_flag <= 1'b0;
         end
         else if (rx_valid_pulse) begin
-            // LOGIC: If incoming data is NOT zero, load it as a SEED.
-            // If incoming data IS zero, STEP the Rule 90 Automaton.
             if (data_in != 64'd0) begin
-                q <= data_in; // Load Seed
+				q <= data_in;     // sending the key that received from rp2040.
             end else begin
-                // Rule 90 Step
+                // Rule-90
                 q <= ({q[62:0],q[63]}) ^ ({q[0],q[63:1]});
             end
-            output_flag <= ~output_flag; // Toggle LED to show activity
+            output_flag <= ~output_flag; // LED toggle to show activity
         end
     end
     
-	assign q_out = {62'd0,q[32:31]};
+	assign q_out = {62'd0,q[32:31]};           //Sending the leak signal 
 	assign led_sig = output_flag;
 
 endmodule
