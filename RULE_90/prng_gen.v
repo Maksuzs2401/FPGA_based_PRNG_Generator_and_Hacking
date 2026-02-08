aa9b1761b7fe765c864da7cfe3e3e8656461bea2
Sujal Makwana <114184382+Maksuzs2401@users.noreply.github.com>
2026-02-08 13:22:16 +0530

    prng_gen.v

--- RULE_90/prng_gen.v
+++ RULE_90/prng_gen.v
@@ -2,31 +2,53 @@ module prng_gen(
     input wire clk,
     input wire reset,
     input [63:0] data_in,
-    input wire rx_valid_pulse,  // data valid signal
-	output wire [63:0] q_out,  // final output to the rp2040
-    output wire led_sig    
+    input wire rx_valid_pulse, 
+    output wire [63:0] q_out,  
+    output wire led_sig
 );
-
-    reg [63:0] q;
+	// Creating the Buffer regs
+    reg [63:0] q, data_in_reg;
     reg output_flag;
+    wire [63:0] q_new, q_left, q_right;
+    reg rx_valid_buff, key_non_zero;
+    
+    // Pre-Computing the Rule-90 logic
+    assign q_left = {q[62:0],q[63]};
+    assign q_right = {q[0],q[63:1]};
+    assign q_new = q_left ^ q_right;
+
+	// Capture Inputs & Pre-Calculate the Comparator
+    always @(posedge clk or negedge reset)begin
+    		if(!reset)begin
+    			data_in_reg <= 64'd0;
+    			rx_valid_buff <= 0;
+    			key_non_zero <= 0;
+    		end
+    		else begin
+    			data_in_reg <= data_in;
+    			rx_valid_buff <= rx_valid_pulse;
+    			key_non_zero <= |data_in;  // The "|" operator (Reduction OR) checks if ANY bit is 1.
+    		end
+    end
 
     always @(posedge clk or negedge reset) begin
         if(!reset) begin
             q <= 64'd0;
             output_flag <= 1'b0;
         end
-        else if (rx_valid_pulse) begin
-            if (data_in != 64'd0) begin
-				q <= data_in;     // sending the key that received from rp2040.
+        else if (rx_valid_buff) begin
+            
+            if (key_non_zero) begin
+                q <= data_in; // Load the key
             end else begin
-                // Rule-90
-                q <= ({q[62:0],q[63]}) ^ ({q[0],q[63:1]});
+                // Rule 90 
+                q <= q_new;
             end
-            output_flag <= ~output_flag; // LED toggle to show activity
+            output_flag <= ~output_flag; // Toggle LED
         end
     end
     
-	assign q_out = {62'd0,q[32:31]};           //Sending the leak signal 
+	assign q_out = {62'd0,q[32:31]};
 	assign led_sig = output_flag;
 
 endmodule
